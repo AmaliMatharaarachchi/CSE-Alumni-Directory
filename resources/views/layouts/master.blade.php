297f85f43f3c7c3a7a8e42275932bc03c4adecfa
Amali Lakshika <maamalilakshika@gmail.com>
2017-01-16 15:02:08 +0530

    Initial commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>@yield('title')</title>
+    @yield('head')
+</head>
+<body class="bg-1">
+@yield('body')
+
+<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="js/jquery.nicescroll.min.js"></script>
+<script type="text/javascript" src="js/jquery.matchHeight-min.js"></script>
+<script type="text/javascript" src="js/jquery.superslides.min.js"></script>
+<script type="text/javascript" src="js/jquery.fs.boxer.min.js"></script>
+<script type="text/javascript" src="js/jquery.easing.js"></script>
+<script type="text/javascript" src="js/jquery.main.js"></script>
+<!-- / settings_box -->
+<script type="text/javascript" src="settings_box/settings_box.js"></script>
+</body>
+</html>
